b49e2483d9e350e421fab9aeb16be7558ac39c01
Carl Sapp <carl@enapragma.co>
2025-11-06 23:06:20 -0600

    Update Google Tag Manager ID to GTM-T6DGH55P

--- app/layout.tsx
+++ app/layout.tsx
@@ -125,7 +125,7 @@ export default function RootLayout({
         />
         <script
           dangerouslySetInnerHTML={{
-            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-MH3X9NDQ');`,
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-T6DGH55P');`,
           }}
         />
         <style>{`
@@ -152,7 +152,7 @@ html {
       <body>
         <noscript>
           <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-MH3X9NDQ"
+            src="https://www.googletagmanager.com/ns.html?id=GTM-T6DGH55P"
             height="0"
             width="0"
             style={{ display: "none", visibility: "hidden" }}
